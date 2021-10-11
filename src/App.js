af09a316b4ed238265e2a6c8365c8f67b3564b47
lirabreno <github.lira@outlook.com>
2021-10-10 22:48:08 -0300

    revert

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ import Profile from './components/profile/profile'
 import Homepage from './components/homepage/homepage'
 
 import './App.scss';
+<<<<<<< HEAD
 import './components/forms.scss'
 import './index.scss';
+=======
+import './index.scss'
+import './authLogin/styleForm.scss'
+>>>>>>> parent of 749e7f4 (test heroku)
   
 function App() {
   return (
